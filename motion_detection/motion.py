b0738047f3f146538025b8ea2c2595109739958a
Klaus Harris <klasharr@users.noreply.github.com>
2020-03-02 17:49:01 +0000

    Making robust

--- motion_detection/motion.py
+++ motion_detection/motion.py
@@ -7,50 +7,78 @@ from gpiozero import LED
 import httplib, urllib
 from ratelimit import limits
 
-logging.basicConfig(level=logging.INFO, filename='motion.log')
+# =====================================================
 
-INPUT_PIN = 18
-red_led = LED(21)
+MOTION_LOGFILE = 'motion.log'
 
+CALLS_PER_PERIOD_LIMIT = 1
+RATE_LIMIT_PERIOD_SECS = 20
+
+ENABLE_PUSHOVER_NOTIFICATIONS = False
+PUSHOVER_TOKEN = 'XXXXX'
+PUSHOVER_USER = 'XXXXX'
+PUSHOVER_SOUND = 'siren'
+PUSHOVER_MESSAGE = 'Movement detected'
+
+PIR_PIN = 18
+LED_PIN = 21
+
+EVERY_X_SECS = realtime.Interval(5.0)
+LOOP_INTERVAL_IN_SECS = 1.5
+
+# =====================================================
+
+logging.basicConfig(level=logging.INFO, filename=MOTION_LOGFILE)
+
+red_led = LED(LED_PIN)
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(INPUT_PIN, GPIO.IN)
+GPIO.setup(PIR_PIN, GPIO.IN)
 
-every_5_secs = realtime.Interval(5.0)
-loop_interval_in_secs = 1.5
 
-@limits(calls=1, period=15)
-def send_message():
+@limits(calls=CALLS_PER_PERIOD_LIMIT, period=RATE_LIMIT_PERIOD_SECS)
+def send_pushover_message():
+    
+    if not ENABLE_PUSHOVER_NOTIFICATIONS: 
+        print('Pushover message would have been sent')
+        return
+
     try:
         conn = httplib.HTTPSConnection("api.pushover.net:443", timeout=3)
         conn.request("POST", "/1/messages.json",
         urllib.urlencode({
-            "token": "XXXXX",
-            "user": "XXXXXX",
-            "sound": "siren",
-            "message": 'movement',
+            "token": PUSHOVER_TOKEN,
+            "user": PUSHOVER_USER,
+            "sound": PUSHOVER_SOUND,
+            "message": PUSHOVER_MESSAGE,
         }), { "Content-type": "application/x-www-form-urlencoded" })
         response = conn.getresponse()
-        print('Ppushover message sent')
+        
+        print('Pushover message sent')
     except Exception as e:
         print('Pushover message failed: ' + str(e))
 
 def update(elapsed_time, delta_time):
 
     red_led.off()
-
-    input_state = GPIO.input(INPUT_PIN)
+    input_state = GPIO.input(PIR_PIN)
+    
     if input_state == True:
         now = datetime.now()
-        dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
-        message = "Motion detected - " + dt_string
+	dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+	message = "Motion detected - " + dt_string
         print(message)
         logging.info(message)
-        send_message()
+        try: 
+            send_pushover_message()
+        except Exception as e:
+            print(str(e))
+        
         red_led.on()
 
-    if every_5_secs.should(delta_time):
+    if EVERY_X_SECS.should(delta_time):
         print(".")
-
+        
+# Visual signal our script started        
 red_led.blink(0.2,0.2,3)
 
-realtime.loop(update, loop_interval_in_secs)
+realtime.loop(update, LOOP_INTERVAL_IN_SECS)
